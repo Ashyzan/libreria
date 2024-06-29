eb8ab5eb3edcfe12398d646abff92f1d644884d7
RosaCielo <ashyzan@gmail.com>
2024-06-29 13:02:49 +0200

    lecture

--- src/main/java/it/ashyzan/booklyb/libreria/controller/BookController.java
+++ src/main/java/it/ashyzan/booklyb/libreria/controller/BookController.java
@@ -1,12 +1,15 @@
 package it.ashyzan.booklyb.libreria.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.ashyzan.booklyb.libreria.model.Book;
 import it.ashyzan.booklyb.libreria.repository.BookRepository;
@@ -20,12 +23,36 @@ public class BookController {
 	private BookRepository repository;
 	
 	@GetMapping
-	public String index(Model model) {
+	public String index(Model model, @RequestParam(name = "author", required = false) String author) {
 		
-		List<Book> libri = repository.findAll();
+		
+		//List<Book> libri = repository.findAll();
+		List<Book> libri = new ArrayList<>();
+		
+		if( author == null || author.isBlank()) {
+			
+			libri = repository.findAll();
+			
+		}
+		
+		else {
+			libri = repository.findByAuthorOrderByIdDesc(author);
+		}
+		
 		
 		model.addAttribute("list", libri);
 		
 		return "/books/index";
+	}
+	
+	// @PathVariable si mette perché nell'url del GetMapping inserisco l'id fra parentesi graffe,
+	// senza il metodo query chiave = valore (esempio http://localhost:8080/books/show?author=pippo&isbn=123456
+	// -> controlla il funzionamento all'url: http://localhost:8080/books/show/1
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable("id") Integer idLibro, Model model) {
+		
+		model.addAttribute("book", repository.getReferenceById(idLibro));
+		
+		return "/books/show";
 	}
 }
--- src/main/java/it/ashyzan/booklyb/libreria/repository/BookRepository.java
+++ src/main/java/it/ashyzan/booklyb/libreria/repository/BookRepository.java
@@ -1,11 +1,25 @@
 package it.ashyzan.booklyb.libreria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.ashyzan.booklyb.libreria.model.Book;
 
 
 	public interface BookRepository extends JpaRepository<Book, Integer> {
+		
+		/*
+		 * SELECT * 
+		 * FROM book b
+		 * WHERE b.author = ?
+		 * 
+		 */
+		
+		
+		public List<Book> findByAuthor(String author);
+		
+		public List<Book> findByAuthorOrderByIdDesc(String author);
 	}
 	
 	
